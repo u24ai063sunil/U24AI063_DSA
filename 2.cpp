e805ef151a8c2a28a5540a9a28d94ca5d4050f05
u24ai063sunil <u24ai063@aid.svnit.ac.in>
2025-03-17 11:24:45 +0530

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+const int MAX_SIZE = 1000;  // Define max size for temp array
+int temp[MAX_SIZE];         // Declare temp array globally
+
+void merge(int arr[], int left, int mid, int right) {
+    int i = left, j = mid + 1, k = left;
+
+    while (i <= mid && j <= right) {
+        if (arr[i] <= arr[j]) {
+            temp[k] = arr[i];
+            i++;
+        } else {
+            temp[k] = arr[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i <= mid) {
+        temp[k] = arr[i];
+        i++;
+        k++;
+    }
+
+    while (j <= right) {
+        temp[k] = arr[j];
+        j++;
+        k++;
+    }
+
+    // Copy back the merged elements into the original array
+    for (int x = left; x <= right; x++) {
+        arr[x] = temp[x];
+    }
+}
+
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    mergeSort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
